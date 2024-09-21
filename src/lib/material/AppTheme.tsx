376658db31b209d0239b8c72cf8cd785625e2643
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-20 23:44:24 -0300

    feat: Add AppTheme component for custom theming

---
+++ src/lib/material/AppTheme.tsx
@@ -0,0 +1,45 @@
+import type { ThemeOptions } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import * as React from "react";
+
+interface AppThemeProps {
+  children: React.ReactNode;
+  /**
+   * This is for the docs site. You can ignore it or remove it.
+   */
+  disableCustomTheme?: boolean;
+  themeComponents?: ThemeOptions["components"];
+}
+
+export default function AppTheme({
+  children,
+  disableCustomTheme,
+  themeComponents,
+}: AppThemeProps) {
+  const theme = React.useMemo(() => {
+    return disableCustomTheme
+      ? {}
+      : createTheme({
+          // For more details about CSS variables configuration, see https://mui.com/material-ui/customization/css-theme-variables/configuration/
+          cssVariables: {
+            colorSchemeSelector: "data-mui-color-scheme",
+            cssVarPrefix: "template",
+          },
+          colorSchemes: {
+            light: true,
+            dark: true,
+          },
+          components: {
+            ...themeComponents,
+          },
+        });
+  }, [disableCustomTheme, themeComponents]);
+  if (disableCustomTheme) {
+    return <React.Fragment>{children}</React.Fragment>;
+  }
+  return (
+    <ThemeProvider theme={theme} disableTransitionOnChange>
+      {children}
+    </ThemeProvider>
+  );
+}
