d39a2f29e75a382844d54025c5cbb2383613cc2d
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 14:39:58 -0300

    refactor: Remove UserProvider from AppProviders

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 
-import { UserProvider } from "../hooks/useUsers";
 import AppTheme from "../lib/material/AppTheme";
 import { queryClient } from "../lib/query-client";
 import { router } from "../routes";
@@ -10,9 +9,7 @@ export const AppProviders = ({ ...props }) => {
   return (
     <AppTheme {...props}>
       <QueryClientProvider client={queryClient}>
-        <UserProvider>
-          <RouterProvider router={router} />
-        </UserProvider>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </AppTheme>
   );
