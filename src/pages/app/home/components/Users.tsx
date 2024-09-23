1d95995bd7b354e7affe3fef75ad895005bcdc52
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 14:54:31 -0300

    refactor: Update Users component to use MUI Table for displaying user data

--- src/pages/app/home/components/Users.tsx
+++ src/pages/app/home/components/Users.tsx
@@ -1,11 +1,56 @@
-import { useQuery } from "@tanstack/react-query";
+import { Delete, Edit } from "@mui/icons-material";
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
-import { getUsers } from "../../../../api/users/get-users";
-import { USERS } from "../../../../constants/queries";
+import { useGetUsers } from "../../../../hooks/useGetUsers";
 
-export const useGetUsers = () => {
-  return useQuery({
-    queryKey: [USERS],
-    queryFn: () => getUsers(),
-  });
+export const Users = () => {
+  const { data: users, isLoading } = useGetUsers({ _page: 1, _limit: 10 });
+  console.log("🔥 ~ Users ~ users:", users);
+
+  if (isLoading) return <div>Carregando...</div>;
+
+  return (
+    <TableContainer component={Paper} sx={{ mt: 4 }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nome</TableCell>
+            <TableCell>CPF</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Telefone</TableCell>
+            <TableCell>Endereço</TableCell>
+            <TableCell>Ações</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users?.data?.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.cpf}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{user.address}</TableCell>
+              <TableCell>
+                <IconButton color="primary">
+                  <Edit />
+                </IconButton>
+                <IconButton color="secondary">
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
