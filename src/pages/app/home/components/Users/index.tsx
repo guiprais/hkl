96a854f67fe4be9e256da6cec59dfd350f1281c3
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 16:02:15 -0300

    refactor: Add dialog for editing user information and implement edit functionality

--- src/pages/app/home/components/Users/index.tsx
+++ src/pages/app/home/components/Users/index.tsx
@@ -1,5 +1,8 @@
 import { Delete, Edit } from "@mui/icons-material";
 import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
   IconButton,
   Paper,
   Table,
@@ -9,52 +12,80 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import { useState } from "react";
 
 import { useDeleteUser } from "../../../../../hooks/useDeleteUser";
 import { useGetUsers } from "../../../../../hooks/useGetUsers";
+import { IUser } from "../../../../../types/user";
+import { UserForm } from "../UserForm";
 
 export const Users = () => {
   const { data: users, isLoading } = useGetUsers();
   const { mutateAsync: deleteUser } = useDeleteUser();
 
+  const [open, setOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+
+  const handleOpen = (user: IUser) => {
+    setCurrentUser(user);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   if (isLoading) return <div>Carregando...</div>;
 
   return (
-    <TableContainer component={Paper} sx={{ mt: 4 }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell>CPF</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Telefone</TableCell>
-            <TableCell>Endereço</TableCell>
-            <TableCell>Ações</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users?.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.cpf}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>{user.phone}</TableCell>
-              <TableCell>{user.address}</TableCell>
-              <TableCell>
-                <IconButton color="primary">
-                  <Edit />
-                </IconButton>
-                <IconButton
-                  color="secondary"
-                  onClick={() => deleteUser(user.id)}
-                >
-                  <Delete />
-                </IconButton>
-              </TableCell>
+    <>
+      <TableContainer component={Paper} sx={{ mt: 4 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>CPF</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Telefone</TableCell>
+              <TableCell>Endereço</TableCell>
+              <TableCell>Ações</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {users?.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.cpf}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.phone}</TableCell>
+                <TableCell>{user.address}</TableCell>
+                <TableCell>
+                  <IconButton color="primary" onClick={() => handleOpen(user)}>
+                    <Edit />
+                  </IconButton>
+                  <IconButton
+                    color="secondary"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    <Delete />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        fullWidth={currentUser ? true : false}
+      >
+        <DialogTitle>Editar Usuário</DialogTitle>
+        <DialogContent>
+          <UserForm user={currentUser ?? undefined} closeDialog={handleClose} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
