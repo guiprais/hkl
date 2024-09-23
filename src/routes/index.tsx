3af6fb0f8b18788725afdc7cba9d38b507254925
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 17:24:24 -0300

    refactor: Update routes to include login route in AUTH object and add PRIVATE route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,27 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
+import { ROUTES } from "../constants/routes";
 import { Home } from "../pages/app/home";
 import { SignIn } from "../pages/auth/SignIn";
+import { RoutesGuard } from "./routes-guard";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <SignIn />,
-  },
-  {
-    path: "/app",
-    element: <Home />,
+    element: (
+      <RoutesGuard>
+        <Outlet />
+      </RoutesGuard>
+    ),
+    children: [
+      {
+        path: ROUTES.AUTH.LOGIN,
+        element: <SignIn />,
+      },
+      {
+        path: ROUTES.PRIVATE,
+        element: <Home />,
+      },
+    ],
   },
 ]);
