0ed35d3d63509cfd838484e3732a7bf789db31ea
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 12:41:35 -0300

    feat: Add form schema for SignIn page

---
+++ src/pages/auth/SignIn/form-schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, "O email é obrigatório")
+    .email("Por favor, insira um email válido"),
+  password: z.string().min(1, "A senha é obrigatória"),
+});
+
+export type FormSchema = z.infer<typeof formSchema>;
