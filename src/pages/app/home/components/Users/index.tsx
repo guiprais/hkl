3611f195c8416bb1bdea812f737690d85125e705
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 15:25:07 -0300

    refactor: Update Users component to use lazy loading for fetching user data

---
+++ src/pages/app/home/components/Users/index.tsx
@@ -0,0 +1,55 @@
+import { Delete, Edit } from "@mui/icons-material";
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+import { useGetUsers } from "../../../../../hooks/useGetUsers";
+
+export const Users = () => {
+  const { data: users, isLoading } = useGetUsers();
+
+  if (isLoading) return <div>Carregando...</div>;
+
+  return (
+    <TableContainer component={Paper} sx={{ mt: 4 }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nome</TableCell>
+            <TableCell>CPF</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Telefone</TableCell>
+            <TableCell>Endereço</TableCell>
+            <TableCell>Ações</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users?.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.cpf}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{user.address}</TableCell>
+              <TableCell>
+                <IconButton color="primary">
+                  <Edit />
+                </IconButton>
+                <IconButton color="secondary">
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
