884e92551e60c3fa7e33a153630ea46adb3dca50
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 17:24:20 -0300

    refactor: Add PasswordProvider for managing password state

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,6 +2,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 
+import { PasswordProvider } from "../hooks/usePassword";
 import AppTheme from "../lib/material/AppTheme";
 import { queryClient } from "../lib/query-client";
 import { router } from "../routes";
@@ -10,8 +11,10 @@ export const AppProviders = ({ ...props }) => {
   return (
     <AppTheme {...props}>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <Toaster richColors />
+        <PasswordProvider>
+          <RouterProvider router={router} />
+          <Toaster richColors />
+        </PasswordProvider>
       </QueryClientProvider>
     </AppTheme>
   );
