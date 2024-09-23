efc32b1cbd56bec4ac250b440e8ba0033ed037e5
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 14:54:36 -0300

    refactor: Update getUsers function to accept optional pagination parameters

--- src/api/users/get-users.ts
+++ src/api/users/get-users.ts
@@ -1,8 +1,11 @@
 import { api } from "../../lib/axios";
+import { IPaginatedParams } from "../../types/paginated-params";
 import { IPaginatedResponse } from "../../types/paginated-response";
 import { IUser } from "../../types/user";
 
-export async function getUsers() {
-  const { data } = await api.get<IPaginatedResponse<IUser[]>>("/user", {});
+export async function getUsers(params?: IPaginatedParams) {
+  const { data } = await api.get<IPaginatedResponse<IUser[]>>("/users", {
+    params,
+  });
   return data;
 }
