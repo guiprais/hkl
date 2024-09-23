481b55625c5236280c9c86d19c31d3553f760ec3
Guilherme de Prais <guilherme.prais@gmail.com>
2024-09-23 12:44:06 -0300

    chore: Refactor SignIn page components into separate files

---
+++ src/pages/auth/SignIn/components/SignInContainer.tsx
@@ -0,0 +1,21 @@
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+export const SignInContainer = styled(Stack)(({ theme }) => ({
+  padding: 20,
+  marginTop: "10vh",
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
+}));
--- src/pages/auth/SignIn/index.tsx
+++ src/pages/auth/SignIn/index.tsx
@@ -13,44 +13,9 @@ import { Controller, useForm } from "react-hook-form";
 import AppTheme from "../../../lib/material/AppTheme";
 import ColorModeSelect from "../../../lib/material/ColorModeSelect";
 import ForgotPassword from "../ForgotPassword";
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
-  },
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  ...theme.applyStyles("dark", {
-    boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
-  }),
-}));
-
-const SignInContainer = styled(Stack)(({ theme }) => ({
-  padding: 20,
-  marginTop: "10vh",
-  "&::before": {
-    content: '""',
-    display: "block",
-    position: "absolute",
-    zIndex: -1,
-    inset: 0,
-    backgroundImage:
-      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-    backgroundRepeat: "no-repeat",
-    ...theme.applyStyles("dark", {
-      backgroundImage:
-        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-    }),
-  },
-}));
+import { Card } from "./components/Card";
+import { SignInContainer } from "./components/SignInContainer";
+import { FormSchema, formSchema } from "./form-schema";
 
 export function SignIn(props: { disableCustomTheme?: boolean }) {
   const { control, handleSubmit } = useForm<FormSchema>({
